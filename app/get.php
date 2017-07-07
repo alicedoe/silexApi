db3184dcfc82e6c4ae6c852667fda412e9964009
alice.edn@gmail.com <alice.edn@gmail.com>
2017-07-07 20:06:52 +0200

    Geocodage ok

--- app/get.php
+++ app/get.php
@@ -1,16 +1,5 @@
 <?php
 
-//$app->get('/', function () use ($app) {
-//    $articles = $app['dao.infosContributeur']->findAll();
-//    ob_start();             // start buffering HTML output
-//    require '../views/view.php';
-//    $view = ob_get_clean(); // assign HTML output to $view
-//    return $view;
-//});
-
-// Home page
-
-
 $app->GET('/alicegabbana/circus/1.0.0/users/{id}/allfavoris', function () use ($app) {
 
 
@@ -58,22 +47,24 @@ $app->GET('/alicegabbana/circus/1.0.0/login', function(Application $app, Request
     return new Response('How about implementing login as a GET method ?');
 });
 
-$app->get('/markermap', "Models\Controller\ApiController::getUserWithCoord");
+$app->get('/markermap', "Models\Controller\GetController::getUserWithCoord");
 
-$app->get('/search/{word}', "Models\Controller\ApiController::getDechetsSearch")->value('word', null );
+$app->get('/search/{word}', "Models\Controller\GetController::getDechetsSearch")->value('word', null );
 
-$app->get('/user/{id}/contribution', "Models\Controller\ApiController::getIdeeFromContri")->value('id', null);
+$app->get('/user/{id}/contribution', "Models\Controller\GetController::getIdeeFromContri")->value('id', null);
 
-$app->get('/useractivated/{id}', "Models\Controller\ApiController::getUserActivated")->value('id', null);
+$app->get('/useractivated/{id}', "Models\Controller\GetController::getUserActivated")->value('id', null);
 
-$app->get('/user/{id}/allfavoris', "Models\Controller\ApiController::getUserFavourite")->value('id', null);
+$app->get('/user/{id}/allfavoris', "Models\Controller\GetController::getUserFavourite")->value('id', null);
 
-$app->get('/user/{id}/companyfavoris', "Models\Controller\ApiController::getUserCompanyFavourite")->value('id', null);
+$app->get('/user/{id}/companyfavoris', "Models\Controller\GetController::getUserCompanyFavourite")->value('id', null);
 
-$app->get('/user/{id}/productfavoris', "Models\Controller\ApiController::getUserProductFavourite")->value('id', null);
+$app->get('/user/{id}/productfavoris', "Models\Controller\GetController::getUserProductFavourite")->value('id', null);
 
-$app->get('/user/{id}', "Models\Controller\ApiController::getUser")->value('id', null);
+$app->get('/user/{id}', "Models\Controller\GetController::getUser")->value('id', null);
 
-$app->get('/levenshtein/{word}', "Models\Controller\ApiController::getLevenshtein")->value('word', null);
+$app->get('/levenshtein/{word}', "Models\Controller\GetController::getLevenshtein")->value('word', null);
 
-$app->get('/contribution', "Models\Controller\ApiController::getAllContribution");
+$app->get('/contribution', "Models\Controller\GetController::getAllContribution");
+
+$app->get('/geocodage', "Models\Controller\GetController::getGeocodage");
--- src/Controller/GetController.php
+++ src/Controller/GetController.php
@@ -4,7 +4,7 @@ namespace Models\Controller;
 
 use Silex\Application;
 
-class ApiController {
+class GetController {
 
     /**
      * API articles controller.
@@ -222,5 +222,44 @@ class ApiController {
 
         return $app->json($contrib);
     }
+
+    /**
+     * Transforme l'adresse en lontitude lattitude
+     *
+     * l'adresse doit être indiqué via le headers
+     *
+     * @param void
+     *
+     * @return array $results & $result
+     */
+
+    function getGeocodage(Application $app) {
+
+        $adresse_complete = $_SERVER['HTTP_ADRESSE'];
+        $adresse_complete = utf8_encode($adresse_complete);
+
+            $url="http://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($adresse_complete)."&oe=utf8";
+            $json = file_get_contents($url);
+            $coordonates = json_decode($json);
+            $latitude = $coordonates->{"results"}[0]->{'geometry'}->{'location'}->{'lat'};
+            $longitude = $coordonates->{"results"}[0]->{'geometry'}->{'location'}->{'lng'};
+
+            if(empty($latitude) or empty($longitude))
+            {
+                $mapquest = "https://www.mapquestapi.com/geocoding/v1/address?key=HbirHr2IRlmgqfkZT2cUERoLxOMaakzi&inFormat=kvp&outFormat=json&location=".urlencode($adresse_complete)."&thumbMaps=false";
+                $json = file_get_contents($mapquest);
+                $coordonates = json_decode($json);
+                $latitude_mapquest = $coordonates->{'results'}[0]->{'locations'}[0]->{'displayLatLng'}->{'lat'};
+                $longitude_mapquest = $coordonates->{'results'}[0]->{'locations'}[0]->{'displayLatLng'}->{'lng'};
+                $latitude=$latitude_mapquest;
+                $longitude=$longitude_mapquest;
+            }
+            $results="";
+
+            $results['latitude'] = $latitude;
+            $results['longitude'] = $longitude;
+        return $app->json($results);
+
+        }
 
 }
